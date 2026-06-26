80c6b4975dac7086e64bccf45a1b87988d85fc93
bas <jaranchai.to@rmuti.ac.th>
2026-06-26 22:32:37 +0700

    editscheema

---
+++ Backend/prisma/migrations/20260626152827_add/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `Is_Read` on the `chats` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `chats` DROP COLUMN `Is_Read`;
