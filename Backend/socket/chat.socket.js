074074acca8c4e182f36d635640f82747ff5b2c2
bas <jaranchai.to@rmuti.ac.th>
2026-07-08 12:02:49 +0700

    refactor send_message handler to streamline message emission and remove unnecessary chat creation logic

--- Backend/socket/chat.socket.js
+++ Backend/socket/chat.socket.js
@@ -9,25 +9,6 @@ export const chatSocket = (io) => {
     );
     socket.on("send_message", async (data) => {
       try {
-        let orderId = data.Order_Id ? Number(data.Order_Id) : null;
-        if (data.Type === "PRICE_OFFER" || data.Type === "IMAGE") {
-          io.to(data.Room_Id).emit("receive_message", data);
-          return;
-        }
-        const chat = await prisma.chats.create({
-          data: {
-            Service_Id: Number(data.Service_Id),
-            Room_Id: data.Room_Id,
-            Order_Id: orderId,
-            Sender_Id: Number(data.Sender_Id),
-            Receiver_Id: Number(data.Receiver_Id),
-            Type: data.Type || "MESSAGE",
-            Message: data.Message || null,
-            Image: data.Image || null,
-            Price: data.Price ? Number(data.Price) : null,
-            Work_Date: data.Work_Date ? new Date(data.Work_Date) : null
-          }
-        });
         io.to(data.Room_Id).emit("receive_message", chat);
       } catch (err) {
         throw err;
