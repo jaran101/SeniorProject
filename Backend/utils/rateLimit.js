4e087bdc3072e9e7f07a77421b8de5e8685bc0b0
bas <jaranchai.to@rmuti.ac.th>
2026-07-08 19:25:58 +0700

    refactor: Update login rate limiter to use custom error handler

--- Backend/utils/rateLimit.js
+++ Backend/utils/rateLimit.js
@@ -1,7 +1,10 @@
 import rateLimit from "express-rate-limit"
+import createError from "./createError.js"
 export const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 5,
-  message: "You have attempted to log in too many times. Please wait 15 minutes and try again",
   headers: true,
+  handler:(req,res,next)=>{
+    next(createError(429,"You have attempted to log in too many times. Please wait 15 minutes and try again"))
+  }
 });
